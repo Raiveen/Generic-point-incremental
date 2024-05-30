6cfd6bce981ecb1d0ba85b02d4cac32ad553089f
Raiveen <111378630+Raiveen@users.noreply.github.com>
2024-05-30 14:17:36 +0200

    tak snad

---
+++ main.js
@@ -0,0 +1,97 @@
+//----------------------==========================-----------------------
+//----------------------==========VARIABLES==========-----------------------
+//----------------------==========================-----------------------
+
+var player = {
+    points: 1000,
+    prestige: 0,
+    prestigerewards: [0, 0]
+};
+
+//----------------------==========================-----------------------
+//----------------------==========UPDATING==========-----------------------
+//----------------------==========================-----------------------
+
+function format(value) {
+    return new Intl.NumberFormat('th-TH', { 
+        minimumFractionDigits: 2, 
+        maximumFractionDigits: 2 
+    }).format(value);
+}
+
+function update() {
+    document.getElementById("points").innerHTML = format(player.points) + " points";
+    document.getElementById("prestigecount").innerHTML = "Prestige: " + player.prestige;
+    document.getElementById("prestige").innerHTML = "Prestige<br>Req: "+format(price)+" points";
+
+    if (player.prestigerewards[0] == 1) {document.getElementById("jedna").style.display = "inline"
+     document.getElementById("jedna").style.backgroundColor = "green";}
+     if (player.prestigerewards[1] == 1) {document.getElementById("dva").style.display = "inline"
+     document.getElementById("jedna").style.backgroundColor = "green";}
+}
+
+//----------------------==========================-----------------------
+//----------------------==========POINT GAIN==========-----------------------
+//----------------------==========================-----------------------
+
+function pointgain() {
+    let x = 0.01;
+    player.points += x;
+    update();
+}
+
+var mainGameLoop = window.setInterval(function() {
+        pointgain();
+}, 10);
+
+//----------------------==========================-----------------------
+//----------------------==========RESET LAYERS==========-----------------------
+//----------------------==========================-----------------------
+var price = 10
+function prestige() {
+    price = 10 * Math.pow(1.75, 2.5*Math.pow(player.prestige+1, 1.5))
+    if (player.points >= price) {
+        player.points -= price;
+        player.prestige++;
+    }
+    if (player.prestige >= 1) player.prestigerewards[0] = 1;
+    if (player.prestige >= 2) player.prestigerewards[0] = 1;
+    update()
+}   
+
+//----------------------==========================-----------------------
+//----------------------==========SAVING AND CLEARING==========-----------------------
+//----------------------==========================-----------------------
+
+function clearData() {
+    localStorage.removeItem("testsave");
+    player = {
+        points: 0,
+        prestige: 0,
+        prestigerewards: [0, 0],
+    };
+    location.reload();
+    update();
+}
+
+function saveGame() {
+    var data = {
+        player: player,
+    };
+    localStorage.setItem("testsave", JSON.stringify(data));
+}
+
+var saveGameLoop = window.setInterval(function() {
+    saveGame();
+}, 1000);
+
+function loadSaveGame() {
+    var savedData = JSON.parse(localStorage.getItem("testsave"));
+    if (savedData !== null) {
+        player = savedData.player !== undefined ? savedData.player : player;
+    }
+    update();
+}
+
+loadSaveGame();
+update();
